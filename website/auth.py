d6ff2bcca0a83bbdb9ebeeddeaa93fb8407c2610
Ákos Benő <akos.beno@gmail.com>
2023-10-07 14:13:00 +0300

    first stable sign up reales without db connecting

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater then 4 characters.', category='error')
+        elif len(firstName) <2:
+            flash('First Name must be greater then 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+           flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created.', category='success')
+            pass
+            
+    
+    return render_template("sign_up.html")
