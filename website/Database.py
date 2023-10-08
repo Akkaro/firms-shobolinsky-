86b3098366dc0109e96407399bdf76f4f8783186
ManyFacedGod1521 <szabi_04@yahoo.com>
2023-10-08 13:29:56 +0300

    news

--- website/Database.py
+++ website/Database.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from pymongo.mongo_client import MongoClient
 import requests
+import pandas as pd
 
 from website.auth import *
 
@@ -45,8 +46,11 @@ def GetDataFromApi():
         "X-RapidAPI-Key": "d200955f50msh92a2f4a24776d7ap18f706jsn899c4532cd92",
         "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com"
     }
-    print("hol vagy")
+
     response = requests.get(url, headers=headers, params=querystring)
+
+    addToDatabase(response)
+
     print(response.json())
 
 
@@ -64,3 +68,36 @@ def GetDataFromDb():
         list.append(fires)
     return list
 
+
+def addToDatabase(news_json):
+    header = ['_type', 'name', 'url', 'image', '_type', 'thumbnail', '_type', 'contentUrl', 'width', 'height', 'description', 'provider', 'datePublished']
+
+    sure_json = news_json.json()
+    df = pd.read_json(sure_json)
+
+    for each in df:
+        row = {}
+        for field in header:
+            row[field] = each[field]
+        data.test.insert_one(row)
+
+
+'''image	
+_type	"ImageObject"
+thumbnail	
+_type	"ImageObject"
+contentUrl	"https://www.bing.com/th?id=OVFT.aSCK8kFAXFx4eYkNn3HrEy&pid=News"
+width	700
+height	466
+description	"As the extreme weather of climate change makes destructive summers like this year's more and more likely, the province’s independent forests watchdog is calling for radical action to make our landscapes more resistant to fire."
+provider	
+0	
+_type	"Organization"
+name	"cbc.ca on MSN.com"
+image	
+_type	"ImageObject"
+thumbnail	
+_type	"ImageObject"
+contentUrl	"https://www.bing.com/th?id=ODF.4NbuPrmIxu0JD126LE1gxQ&pid=news"
+datePublished	"2023-10-07T08:00:00.0000000Z"
+'''
