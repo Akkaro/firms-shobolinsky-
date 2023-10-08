b3665cc4a7766aabd56ec87a97cd70f5aec1ab3d
flora <balogh.flora@yahoo.com>
2023-10-08 13:33:52 +0300

    news design

--- website/views.py
+++ website/views.py
@@ -6,9 +6,15 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
 @views.route('/safety')
 def safety():
     return render_template("safety.html")
+
 @views.route('/map')
 def map():
     return render_template("map.html")
+
+@views.route('/news')
+def news():
+    return render_template("news.html")
